1b920a1ce1fb30335a5928a8e030612bc4086872
hefepa <paulorimogunje@gmail.com>
2022-06-05 17:39:26 -0700

    done

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 
-import add_0
+if __name__ == "__main__":
+    from add_0 import add
+    def add(a,b):
+        a = 1
+        b = 2
+        print(f'{0} + {1} = {2}'.format(a, b, a + b))
